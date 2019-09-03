46630e19b5b3329b151f302da36233588e7961b6
Shivam Barthwal <shivambarthwal@live.com>
2019-09-03 18:37:55 +0200

    The ToDo List Spring Java App

---
+++ src/main/java/com/in28minutes/todo/rest/TodoRestController.java
@@ -0,0 +1,34 @@
+package com.in28minutes.todo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.model.Todo;
+import com.in28minutes.todo.service.TodoService;
+
+/**
+ *
+ * @author Shivam Barthwal
+ */
+
+@RestController
+public class TodoRestController {
+	@Autowired
+	private TodoService service;
+
+	@RequestMapping(value = "/todo/", method = RequestMethod.GET)
+	public List<Todo> listAllTodos() {
+		List<Todo> users = service.retrieveTodos("ut-capitole");
+		return users;
+	}
+
+	@RequestMapping(value = "/todo/{id}", method = RequestMethod.GET)
+	public Todo retrieveTodo(@PathVariable("id") int id) {
+		return service.retrieveTodo(id);
+	}
+}
---
+++ src/main/java/com/in28minutes/todo/service/TodoService.java
@@ -0,0 +1,66 @@
+package com.in28minutes.todo.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.in28minutes.model.Todo;
+
+/**
+ *
+ * @author Shivam Barthwal
+ */
+
+@Service
+public class TodoService {
+	private static List<Todo> todos = new ArrayList<Todo>();
+	private static int todoCount = 3;
+
+	static {
+		todos.add(new Todo(1, "ut-capitole", "Learn Spring MVC", new Date(),
+				false));
+		todos.add(new Todo(2, "ut-capitole", "Learn Struts", new Date(), false));
+		todos.add(new Todo(3, "ut-capitole", "Learn Hibernate", new Date(),
+				false));
+	}
+
+	public List<Todo> retrieveTodos(String user) {
+		List<Todo> filteredTodos = new ArrayList<Todo>();
+		for (Todo todo : todos) {
+			if (todo.getUser().equals(user))
+				filteredTodos.add(todo);
+		}
+		return filteredTodos;
+	}
+
+	public Todo retrieveTodo(int id) {
+		for (Todo todo : todos) {
+			if (todo.getId() == id)
+				return todo;
+		}
+		return null;
+	}
+
+	public void updateTodo(Todo todo) {
+		todos.remove(todo);
+		todos.add(todo);
+	}
+
+	public void addTodo(String name, String desc, Date targetDate,
+			boolean isDone) {
+		todos.add(new Todo(++todoCount, name, desc, targetDate, isDone));
+	}
+
+	public void deleteTodo(int id) {
+		Iterator<Todo> iterator = todos.iterator();
+		while (iterator.hasNext()) {
+			Todo todo = iterator.next();
+			if (todo.getId() == id) {
+				iterator.remove();
+			}
+		}
+	}
+}
